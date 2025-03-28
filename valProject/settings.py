b39e526515ae465c51aba9bf96e0493324494b1a
Daniel <mesadani91@gmail.com>
2025-03-28 11:38:35 +0100

    fix

--- valProject/settings.py
+++ valProject/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'valProject.wsgi.application'
 
 DATABASES = {
   'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': 'railway',
         'USER': 'root',
         'PASSWORD': 'LgRkCRJstseaSJLfMZwSPSEcHlkBLekE',
