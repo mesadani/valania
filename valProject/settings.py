e2f5c0a8d9ee04cf0bbad61afa2da7cfeb6dcafa
Daniel <mesadani91@gmail.com>
2025-03-28 11:35:47 +0100

    fix

--- valProject/settings.py
+++ valProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,9 +91,7 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
+
 
 
 # Password validation
