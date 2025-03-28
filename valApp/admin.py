ffbedcecf8c23ef6fa292c52ed0df14ab71e5277
Daniel <mesadani91@gmail.com>
2025-03-28 10:01:19 +0100

    initial commit

---
+++ valApp/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import (
+    Professions, Crafting, craftingRequirements, Objects, ObjectCategorys, ObjectTypes,
+    Races, Rarities, RolesCombatUnits, CombatUnits, HeroeTypes, Heroes
+)
+
+# Método común para mostrar imágenes
+def image_tag(obj):
+    if obj.image:
+        return format_html('<img src="{}" width="50" height="50" />'.format(obj.image.url))
+    return "No Image"
+image_tag.short_description = 'Image'
+
+class CraftingRequirementsInline(admin.TabularInline):
+    model = craftingRequirements
+    extra = 1
+
+@admin.register(Crafting)
+class CraftingAdmin(admin.ModelAdmin):
+    inlines = [CraftingRequirementsInline]
+
+@admin.register(Objects)
+class ObjectsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'objectType', 'objectCategory', image_tag)
+
+@admin.register(Professions)
+class ProfessionsAdmin(admin.ModelAdmin):
+    list_display = ('name', image_tag)
+
+@admin.register(Races)
+class RacesAdmin(admin.ModelAdmin):
+    list_display = ('name', image_tag)
+
+@admin.register(Heroes)
+class HeroesAdmin(admin.ModelAdmin):
+    list_display = ('name', image_tag)
+
+@admin.register(CombatUnits)
+class CombatUnitsAdmin(admin.ModelAdmin):
+    list_display = ('name', image_tag)
+
+# Registro de modelos sin personalización
+admin.site.register(ObjectTypes)
+admin.site.register(ObjectCategorys)
+admin.site.register(Rarities)
+admin.site.register(HeroeTypes)
+admin.site.register(RolesCombatUnits)
---
+++ valApp/models.py
@@ -0,0 +1,119 @@
+from django.db import models
+
+# Create your models here.
+
+class Professions(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField() 
+    image = models.ImageField(upload_to='professions/images/', blank=True, null=True)  # Nuevo campo de imagen
+
+
+    def __str__(self):
+        return self.name
+    
+class ObjectTypes(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+class ObjectCategorys(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Objects(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    objectType = models.ForeignKey(ObjectTypes, on_delete=models.CASCADE)
+    objectCategory = models.ForeignKey(ObjectCategorys, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='objects/images/', blank=True, null=True)  # Nuevo campo de imagen
+
+
+    def __str__(self):
+        return self.name
+    
+class Crafting(models.Model):
+    object = models.ForeignKey(Objects, on_delete=models.CASCADE)
+    proffesion = models.ForeignKey(Professions, on_delete=models.CASCADE)
+    level = models.IntegerField()
+    quantity = models.IntegerField()
+    probability = models.FloatField()
+    time = models.IntegerField()
+
+    def __str__(self):
+        return self.object.name    
+    
+
+class craftingRequirements(models.Model):
+    craft = models.ForeignKey(Crafting, on_delete=models.CASCADE)
+    object = models.ForeignKey(Objects, on_delete=models.CASCADE)
+    quantity = models.IntegerField() 
+
+
+    def __str__(self):
+        return self.craft.object.name + " - " + self.object.name        
+
+
+class Races(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField() 
+    image = models.ImageField(upload_to='races/images/', blank=True, null=True)  # Nuevo campo de imagen
+
+
+    def __str__(self):
+        return self.name
+    
+class Rarities(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField() 
+    def __str__(self):
+        return self.name    
+    
+class HeroeTypes(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField() 
+    
+    def __str__(self):
+        return self.name 
+
+class Heroes(models.Model):
+    name = models.CharField(max_length=200)
+    type = models.ForeignKey(HeroeTypes, on_delete=models.CASCADE)
+    race = models.ForeignKey(Races, on_delete=models.CASCADE)
+    supply = models.IntegerField()
+    rarity = models.ForeignKey(Rarities, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    location = models.CharField(max_length=200)
+    description = models.TextField()
+
+    image = models.ImageField(upload_to='heroes/images/', blank=True, null=True)  # Nuevo campo de imagen
+
+
+    def __str__(self):
+        return self.name       
+    
+
+class RolesCombatUnits(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField() 
+    
+    def __str__(self):
+        return self.name     
+class CombatUnits(models.Model):
+    name = models.CharField(max_length=200)
+    role = models.ForeignKey(RolesCombatUnits, on_delete=models.CASCADE)
+    race = models.ForeignKey(Races, on_delete=models.CASCADE)
+    supply = models.IntegerField()
+    rarity = models.ForeignKey(Rarities, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    location = models.CharField(max_length=200)
+    troopPoints = models.IntegerField()
+    description = models.TextField()
+
+    image = models.ImageField(upload_to='combatUnits/images/', blank=True, null=True)  # Nuevo campo de imagen
+
+
+    def __str__(self):
+        return self.name     
