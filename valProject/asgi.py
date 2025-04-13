652e772f9d9b84deb71c5d893d0bdc7ba13568ce
Daniel <mesadani91@gmail.com>
2025-04-13 19:24:59 +0200

    fix

--- valProject/asgi.py
+++ valProject/asgi.py
@@ -2,15 +2,16 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-import valProject.routing
-
+from django.urls import path
+from valProject.consumers import NotificationConsumer
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'valProject.settings')
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            valProject.routing.websocket_urlpatterns
-        )
+        URLRouter([
+            path("ws/notifications/", NotificationConsumer.as_asgi()),
+        ])
     ),
 })
